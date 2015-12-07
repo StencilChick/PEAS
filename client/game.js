99b18726eef0435fa00c2b9a92bf6411076dcbf5
Gwendolyn Hart <glh1163@rit.edu>
2015-12-06 23:18:49 -0500

    Things with websockets

--- client/game.js
+++ client/game.js
@@ -1,9 +1,68 @@
 "use strict";
 
-var connectSocket = function(e) {
-	var socket = io.connect();
+// socket stuff
+var player = {
+	x: 0,
+	y: 0
+};
+var players = {};
+
+var socket;
+
+var setupSocket = function() {
+	socket = io.connect();
+	players = {};
 	
 	socket.on('connect', function() {
-		socket.emit('join', {x: 0, y: 0});
+		player.x = Math.floor(Math.random() * 5);
+		player.y = Math.floor(Math.random() * 5);
+		socket.emit('join', {x: player.x, y: player.y});
 	});
+	
+	socket.on('join', function(data) {
+		players[data.player] = data.data;
+	});
+	
+	socket.on('leave', function(data) {
+		delete players[data.player];
+	});
+	
+	socket.on('update', function(data) {
+		
+	});
+}
+
+// display stuff
+var canvas;
+var context;
+
+var setupCanvas = function() {
+	canvas = document.getElementById('game');
+	context = canvas.getContext('2d');
+}
+
+var updateCanvas = function() {
+	// draw all the things
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	context.fillStyle = 'red';
+	var pList = Object.keys(players);
+	for (var i = 0; i < pList.length; i++) {
+		var obj = players[pList[i]];
+		
+		context.fillRect(obj.x*32, obj.y*32, 32, 32);
+	}
+	
+	context.fillStyle = 'green';
+	context.fillRect(player.x*32, player.y*32, 32, 32);
+	
+	// update
+	setTimeout(updateCanvas, 1000/30);
+}
+
+window.onload = function() {
+	setupCanvas();
+	setupSocket();
+	
+	updateCanvas();
 }
--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,50 @@
+var users = {};
+
+
 var configureSockets = function(io) {
 	io.sockets.on('connection', function(socket) {
-		socket.join('map0');
+		// join
+		socket.on('join', function(data) {
+			socket.name = '' + Date.now() + Math.floor(Math.random() * 100);
+			socket.lastData = data;
+			socket.join('map0');
 		
-		socket.on('update', function(data) {
-			socket.broadcast.to('map0').emit('update', data);
+			socket.broadcast.to('map0').emit('join', formatData(socket, data));
+			var uKeys = Object.keys(users);
+			for (var i = 0; i < uKeys.length; i++) {
+				var obj = users[uKeys[i]];
+				
+				socket.emit('join', formatData(obj, obj.lastData));
+			}
+			
+			users[socket.name] = socket;
 		});
 		
-		socket.on('disconnect', function(socket) {
+		// update position
+		socket.on('update', function(data) {
+			if (data.date) {
+				if (data.date < socket.lastData.date) return;
+			}
+			
+			socket.lastData = data;
+			socket.broadcast.to('map0').emit('update', formatData(socket, data));
+		});
+		
+		// disconnect
+		socket.on('disconnect', function() {
+			socket.broadcast.to('map0').emit('leave', formatData(socket, {}));
 			socket.leave('map0');
+			delete users[socket.name];
 		});
 	});
 };
 module.exports.configureSockets = configureSockets;
+
+
+var formatData = function(socket, data) {
+	var date;
+	if (data.date) { date = data.date; }
+	else { date = Date.now(); }
+	
+	return {player: socket.name, date: date, data: data};
+};
